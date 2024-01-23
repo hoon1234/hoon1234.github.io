1bb34f4253810b260fc7791e1a51b257cab5a564
hooni <qudgns964@gmail.com>
2024-01-23 10:05:19 +0900

    20240123 commit

---
+++ modal.js
@@ -0,0 +1,11 @@
+const modalBtn = document.querySelector(".modal-Btn");
+const modal = document.querySelector(".modal");
+
+modalBtn.addEventListener("click", () => {
+    modal.showModal()
+})
+
+const closeBtn = modal.querySelector(".close")
+closeBtn.addEventListener("click", e => {
+    modal.style.display = "none"
+});
